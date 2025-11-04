407c7df9f444f898d932ad9bb9fee6029f10b8aa
rdsalves <s.rodrigo5@pucpr.edu.br>
2025-11-04 00:36:42 -0300

    1:1 completo!

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ void* processo_somar_thread(void* carga) {
     return NULL;
 }
 
-void* fila_processos(void* arg) {
+void* fila_processosNM(void* arg) {
     (void)arg;
     while (1) {
         int meu_indice;
@@ -73,26 +73,65 @@ void* fila_processos(void* arg) {
     return NULL;
 }
 
+void* fila_processos11(void* arg) {
+    int meu_indice = *(int*)arg;
+
+    printf("Thread: %p executando tarefa: %d\n", (void*)pthread_self(), meu_indice);
+    processo_somar_thread(&carga);
+
+    fflush(stdout);
+    return NULL;
+}
+
 
 int main() {
+// =============== MODELO N:M ================================ //
+    // QueryPerformanceFrequency(&frequencia);
+    // LARGE_INTEGER inicioNM;
+    // LARGE_INTEGER fimNM;
+    // pthread_t threadsNM[M];
+    //
+    // QueryPerformanceCounter(&inicioNM);
+    //
+    // for (int i = 0; i < M; ++i) {
+    //     pthread_create(&threadsNM[i], NULL, fila_processosNM, NULL);
+    // }
+    //
+    // for (int i = 0; i < M; ++i) {
+    //     pthread_join(threadsNM[i], NULL);
+    // }
+    //
+    // QueryPerformanceCounter(&fimNM);
+    //
+    // double resultadoNM = (double)(fimNM.QuadPart - inicioNM.QuadPart) / (double)frequencia.QuadPart;
+    // printf("\nmodelo N:M (N:%d, M:%d) - Tempo Total: %.6f", N, M, resultadoNM);
+
+// =============== MODELO 1:1 ================================ //
+
     QueryPerformanceFrequency(&frequencia);
-    LARGE_INTEGER inicio;
-    LARGE_INTEGER fim;
-    pthread_t threads[M];
+    LARGE_INTEGER inicio11;
+    LARGE_INTEGER fim11;
+    pthread_t* threads11 = (pthread_t*)malloc(sizeof(pthread_t) * N);
+    int* indices = (int*)malloc(sizeof(int) * N);
 
-    QueryPerformanceCounter(&inicio);
+    QueryPerformanceCounter(&inicio11);
 
-    for (int i = 0; i < M; ++i) {
-        pthread_create(&threads[i], NULL, fila_processos, NULL);
+    for (int i = 0; i < N; ++i) {
+        indices[i] = i;
+        pthread_create(&threads11[i], NULL, fila_processos11, &indices[i]);
     }
 
-    for (int i = 0; i < M; ++i) {
-        pthread_join(threads[i], NULL);
+    for (int i = 0; i < N; ++i) {
+        pthread_join(threads11[i], NULL);
     }
 
-    QueryPerformanceCounter(&fim);
+    QueryPerformanceCounter(&fim11);
 
-    double resultado = (double)(fim.QuadPart - inicio.QuadPart) / (double)frequencia.QuadPart;
-    printf("\n%.6f", resultado);
+    double resultado11 = (double)(fim11.QuadPart - inicio11.QuadPart) / (double)frequencia.QuadPart;
+    printf("\nmodelo 1:1 (1:%d, 1:%d) - Tempo Total: %.6f", N, N, resultado11);
+
+    free(threads11);
+    free(indices);
+
     return 0;
 }
