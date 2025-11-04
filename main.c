a40d3139f472a91bd0aac5f226c1f0ac3d0cf169
rdsalves <s.rodrigo5@pucpr.edu.br>
2025-11-03 22:14:50 -0300

    NM completo implementado

--- main.c
+++ main.c
@@ -1,50 +1,98 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <time.h>
-#include <profileapi.h>
+#include <windows.h>
 #include <stdint.h>
 
 LARGE_INTEGER frequencia;
+int         N = 100;
+int         M = 4;
+long long   carga = 2147483647;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int proxima_tarefa = 0;
 
-long long processo_somar(int range) {
-    long long total = 0;
+// long long processo_somar(int range) {
+//     long long total = 0;
+//     for (int i = 0; i < range; ++i) {
+//         total += i;
+//     }
+//     return total;
+// }
+
+// double temporizador_cpu_tick(int range) {
+//     clock_t clock_inicio = clock();
+//     long long resultado = processo_somar(range);
+//     clock_t clock_fim = clock();
+//
+//     double tempo = (double)(clock_fim - clock_inicio) / CLOCKS_PER_SEC;
+//
+//     return tempo;
+// }
+
+// double temporizador_relogio_humano(long long range, LARGE_INTEGER frequencia) {
+//     LARGE_INTEGER inicio;
+//     LARGE_INTEGER fim;
+//
+//     QueryPerformanceCounter(&inicio);
+//     long long resultado = processo_somar(range);
+//     QueryPerformanceCounter(&fim);
+//
+//     long long diferenca = fim.QuadPart - inicio.QuadPart;
+//     double tempo = (double)diferenca / (double)frequencia.QuadPart;
+//
+//     return tempo;
+// }
+
+void* processo_somar_thread(void* carga) {
+    long long range = *(long long*)carga;
+    volatile long long total = 0;
     for (int i = 0; i < range; ++i) {
         total += i;
     }
-    return total;
+    return NULL;
 }
 
-double temporizador_cpu_tick(int range) {
-    clock_t clock_inicio = clock();
-    long long resultado = processo_somar(range);
-    clock_t clock_fim = clock();
+void* fila_processos(void* arg) {
+    (void)arg;
+    while (1) {
+        int meu_indice;
 
-    double tempo = (double)(clock_fim - clock_inicio) / CLOCKS_PER_SEC;
+        pthread_mutex_lock(&mutex);
+        if (proxima_tarefa >= N) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        meu_indice = proxima_tarefa++;
+        pthread_mutex_unlock(&mutex);
 
-    return tempo;
-}
+        printf("Thread: %p executando tarefa: %d\n", (void*)pthread_self(), meu_indice);
+        processo_somar_thread(&carga);
 
-double temporizador_relogio_humano(int range, LARGE_INTEGER frequencia) {
-    LARGE_INTEGER inicio;
-    LARGE_INTEGER fim;
-
-    QueryPerformanceCounter(&inicio);
-    long long resultado = processo_somar(range);
-    QueryPerformanceCounter(&fim);
-
-    long long diferenca = fim.QuadPart - inicio.QuadPart;
-    double tempo = (double)diferenca / (double)frequencia.QuadPart;
-
-    return tempo;
+        fflush(stdout);
+    }
+    return NULL;
 }
 
 
 int main() {
     QueryPerformanceFrequency(&frequencia);
+    LARGE_INTEGER inicio;
+    LARGE_INTEGER fim;
+    pthread_t threads[M];
 
-    // double resultado = temporizador_cpu_tick(2147483647);
-    double resultado2 = temporizador_relogio_humano(2147483647, frequencia);
-    // printf("%.6f", resultado);
-    printf("\n%.6f", resultado2);
+    QueryPerformanceCounter(&inicio);
+
+    for (int i = 0; i < M; ++i) {
+        pthread_create(&threads[i], NULL, fila_processos, NULL);
+    }
+
+    for (int i = 0; i < M; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    QueryPerformanceCounter(&fim);
+
+    double resultado = (double)(fim.QuadPart - inicio.QuadPart) / (double)frequencia.QuadPart;
+    printf("\n%.6f", resultado);
     return 0;
 }
